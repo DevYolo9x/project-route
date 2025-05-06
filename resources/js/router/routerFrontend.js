1e23a65699a3cf811709f62bcddb64fb919543eb
Binh TX <xuanbinhtxb@gmail.com>
2025-05-06 23:20:22 +0700

    Cap nhat file

--- resources/js/router/routerFrontend.js
+++ resources/js/router/routerFrontend.js
@@ -3,5 +3,10 @@ export default [
         path: '',
         name: 'home',
         component: () => import('../components/frontend/home/Home.vue')
+    },
+    {
+        path: '',
+        name: 'home',
+        component: () => import('../components/frontend/home/Home.vue')
     }
 ]
